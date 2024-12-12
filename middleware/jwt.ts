708dcb62a01d3c33fb1aa43a22bf562730e17cfb
GeorgeOgeorge <george1bsilva@outlook.com>
2024-12-12 17:38:24 -0300

    feat: adiciona middleware de cors para app

--- middleware/jwt.ts
+++ middleware/jwt.ts
@@ -3,27 +3,17 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import { UserJwt } from '../types/user';
 import appConfig from '../core/app_config';
 
-const ALLOWED_ORIGINS = ["http://localhost:55963"];
-
-export function validateJWT(req: VercelRequest, res: VercelResponse, next: Function) {
-  if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", ALLOWED_ORIGINS.join(","));
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS, PATCH, DELETE, POST, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    return res.status(200).end();
-  }
-
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Missing or invalid token" });
+export function validateJWT(request: VercelRequest, response: VercelResponse, route: Function) {
+  const authHeader = request.headers.authorization;
+  if (!(authHeader?.startsWith("Bearer "))) {
+    return response.status(401).json({ message: "Missing or invalid token" });
   }
 
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, appConfig.jwt.secretKey) as UserJwt;
-    next(decoded);
+    route(decoded);
   } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token", error });
+    return response.status(401).json({ message: "Invalid or expired token", error });
   }
 }
